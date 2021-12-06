72e57ad0823b985984d0f1fd3a9ae75cd11caa3f
akashSharma135 <akashsharma1352000@gmail.com>
2021-12-07 00:40:58 +0530

    added templates and post model

---
+++ README.md
@@ -0,0 +1,16 @@
+# Django Blog App 
+
+## Summary
+
+A blog app where users can upload posts to share with the world.
+
+## Initial Project Setup
+
+```bash
+python -m venv env
+source env/bin/activate
+pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
+python manage.py runserver
+```
---
+++ blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Post
+
+def home(request):
+    
+    return render(
+            request, 
+            'blog/home.html', 
+            context = {
+                'posts': Post.objects.all(),
+                'title': 'home'
+            }
+        )
+
+def about(request):
+    return render(request, 'blog/about.html', )
